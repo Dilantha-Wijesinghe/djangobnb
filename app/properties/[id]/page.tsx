5bbf8d7bc2c7fd248adc63474f00791766eec05e
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-05-04 11:37:41 +0530

    first commit

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+const PropertyDetailPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6">
+      <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          className="object-cover w-full h-full"
+          alt="Beach house"
+        />
+      </div>
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">Property name</h1>
+
+          <span className="mb-6 block text-lg text-gray-600">
+            4 guests - 2 bedrooms - 1 bathroom
+          </span>
+
+          <hr />
+        </div>
+        <div>right</div>
+      </div>
+    </main>
+  );
+};
+
+export default PropertyDetailPage;
